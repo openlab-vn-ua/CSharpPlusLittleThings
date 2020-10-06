87ae9686145316711238cab856ff0a07cac73242
S.Ageyev <justboss777@gmail.com>
2020-10-06 18:22:35 +0300

    Added ObjectCloner

---
+++ src/ObjectCloner.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+
+namespace OpenLab.Plus.SystemPlus
+{
+    public partial class ObjectCloner
+    {
+        /// <summary>
+        /// Copies maching fields and propertites from one object to another (shallow copy)
+        /// Any matching properties are written to the target.
+        /// </summary>
+        /// <param name="Source">The source object</param>
+        /// <param name="Target">The target object</param>
+        /// <param name="TargetMemberIsExcludedPredicate">A function to check should value be copied (default - all)</param>
+        /// <param name="MemberAccess">Reflection binding access for extracting fields</param>
+        public static void MemberwiseCopy(object Source, object Target, BindingFlags MemberAccess = BindingFlags.Public | BindingFlags.Instance, Func<MemberInfo, bool> TargetMemberIsExcludedPredicate = null)
+        {
+            MemberInfo[] targetMembers = Target.GetType().GetMembers(MemberAccess);
+            foreach (MemberInfo TargetMember in targetMembers)
+            {
+                string TargetMemberName = TargetMember.Name;
+
+                if (TargetMemberIsExcludedPredicate != null)
+                {
+                    if (TargetMemberIsExcludedPredicate(TargetMember))
+                    {
+                        continue;
+                    }
+                }
+
+                if (TargetMember.MemberType == MemberTypes.Field)
+                {
+                    FieldInfo TargetField = TargetMember as FieldInfo;
+
+                    if (TargetField == null) { continue; }
+
+                    FieldInfo SourceField = Source.GetType().GetField(TargetMemberName, MemberAccess);
+                    if (SourceField != null)
+                    {
+                        TargetField.SetValue(Target, SourceField.GetValue(Source));
+                        continue;
+                    }
+
+                    PropertyInfo SourceProperty = Source.GetType().GetProperty(TargetMemberName, MemberAccess);
+                    if (SourceProperty != null)
+                    {
+                        if (SourceProperty.CanRead)
+                        {
+                            TargetField.SetValue(Target, SourceProperty.GetValue(Source));
+                        }
+                        continue;
+                    }
+                }
+                else if (TargetMember.MemberType == MemberTypes.Property)
+                {
+                    PropertyInfo TargetProperty = TargetMember as PropertyInfo;
+
+                    if (TargetProperty == null) { continue; }
+                    if (!TargetProperty.CanWrite) { continue; }
+
+                    PropertyInfo SourceProperty = Source.GetType().GetProperty(TargetMemberName, MemberAccess);
+                    if (SourceProperty != null)
+                    {
+                        if (SourceProperty.CanRead)
+                        {
+                            TargetProperty.SetValue(Target, SourceProperty.GetValue(Source));
+                        }
+                        continue;
+                    }
+
+                    FieldInfo SourceField = Source.GetType().GetField(TargetMemberName, MemberAccess);
+                    if (SourceField != null)
+                    {
+                        TargetProperty.SetValue(Target, SourceField.GetValue(Source));
+                        continue;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create an object clone and copies propertites from one object to another (shallow clone)
+        /// </summary>
+        /// <param name="Source">The source object</param>
+        /// <param name="TargetMemberIsExcludedPredicate">A function to check should value be copied (default - all)</param>
+        /// <param name="MemberAccess">Reflection binding access for extracting fields</param>
+        public static T CreateMemberwiseClone<T>(T Source, BindingFlags MemberAccess = BindingFlags.Public | BindingFlags.Instance, Func<MemberInfo, bool> TargetMemberIsExcludedPredicate = null) where T : class, new()
+        {
+            T Result = new T();
+            MemberwiseCopy(Source, Result, MemberAccess, TargetMemberIsExcludedPredicate);
+            return Result;
+        }
+    }
+}
---
+++ test/ObjectClonerShould.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenLab.Plus.UnitTest
+{
+    using OpenLab.Plus.SystemPlus;
+
+    [TestClass]
+    public class ObjectClonerShould
+    {
+        class APair<T>
+        {
+            public T First;
+            public T Second { get; set; }
+        }
+
+        protected void AssertAreEquals(dynamic expected, dynamic actual)
+        {
+            Assert.AreEqual(expected.First, actual.First);
+            Assert.AreEqual(expected.Second, actual.Second);
+        }
+
+        protected Random RNG = new Random();
+
+        protected int RV()
+        {
+            return RNG.Next(1, 1000);
+        }
+
+        protected string RS()
+        {
+            return "S"+RV().ToString();
+        }
+
+        [TestMethod]
+        public void CloneObjects()
+        {
+            //var a = new { First = 101, Second = 102 };
+            //var b = new { First = 201L, Second = 202L };
+
+            if (true) { var A = new APair<int> { First = RV(), Second = RV() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+            if (true) { var A = new APair<long> { First = RV(), Second = RV() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+            if (true) { var A = new APair<float> { First = RV(), Second = RV() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+            if (true) { var A = new APair<double> { First = RV(), Second = RV() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+            if (true) { var A = new APair<decimal> { First = RV(), Second = RV() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+            if (true) { var A = new APair<string> { First = RS(), Second = RS() }; AssertAreEquals(A, ObjectCloner.CreateMemberwiseClone(A)); }
+        }
+
+        [TestMethod]
+        public void CopyObjectsWithFieldsOfAssignableTypes()
+        {
+            var A = new APair<int>() { First = RV(), Second = RV() };
+            var B = new APair<long>();
+            ObjectCloner.MemberwiseCopy(A, B);
+            AssertAreEquals(A, B);
+        }
+
+        [TestMethod]
+        public void CopyFromAnonimousObjectsWithFieldsOfSameTypes()
+        {
+            var A = new { First = (int)RV(), Second = (int)RV() };
+            var B = new APair<int>();
+            ObjectCloner.MemberwiseCopy(A, B);
+            AssertAreEquals(A, B);
+        }
+
+        [TestMethod]
+        public void CopyFromAnonimousObjectsWithFieldsOfAssignableTypes()
+        {
+            var A = new { First = (int)RV(), Second = (short)RV() };
+            var B = new APair<long>();
+            ObjectCloner.MemberwiseCopy(A, B);
+            AssertAreEquals(A, B);
+        }
+
+        [TestMethod]
+        public void FailToCopyFieldsOfUnassignableTypes()
+        {
+            var A = new { First = (int)10, Second = (long)20 };
+            var B = new APair<byte>();
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ObjectCloner.MemberwiseCopy(A, B);
+            });
+        }
+    }
+}
